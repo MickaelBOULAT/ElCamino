b0be177e0d87cd5bd70fffafa9caace6bba46456
Kris78 <hamet_ch@hotmail.fr>
2019-09-27 12:23:16 +0200

    more styles

--- src/index.js
+++ src/index.js
@@ -1,23 +1,30 @@
 /**
  * @file Entry point of the app
  */
-import { Router, globals, logger } from 'splay'
-import Connector from '@connector'
-import '@css/style.less'
+import { Router, globals, logger } from "splay";
+import Connector from "@connector";
+import "@css/style.less";
+import "@css/fonts.less";
 
 // import components asynchronously
-const Example = () => import(/* webpackChunkName: "example" */'./components/example')
-const Detail = () => import(/* webpackChunkName: "detail" */'./components/detail')
+const Example = () =>
+  import(/* webpackChunkName: "example" */ "./components/example");
+const Detail = () =>
+  import(/* webpackChunkName: "detail" */ "./components/detail");
 
-logger.mode = DEBUG
+logger.mode = DEBUG;
 
-function launch () {
+function launch() {
   // init Router
-  Router.init('app', [
-    { re: /^detail/, Component: Detail },
-    { re: /^(.*)$/, Component: Example } // fallback url
-  ], () => globals.isBox && Connector.ready(() => Connector.registerKeys()))
+  Router.init(
+    "app",
+    [
+      { re: /^detail/, Component: Detail },
+      { re: /^(.*)$/, Component: Example } // fallback url
+    ],
+    () => globals.isBox && Connector.ready(() => Connector.registerKeys())
+  );
 }
 
 // active remote control & box communication
-Connector.init().then(launch)
+Connector.init().then(launch);
